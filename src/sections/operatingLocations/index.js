dfdf7c1b6204cc7b42ee9017f7604a474b0d3b4a
João Victor Morais <45773455+jovicpdm@users.noreply.github.com>
2022-06-23 09:49:51 -0300

    add cities

--- src/sections/operatingLocations/index.js
+++ src/sections/operatingLocations/index.js
@@ -20,6 +20,50 @@ const OperatingLocations = () => {
             },
           ]}
         />
+        <CityOperating 
+          city="Nova Ipixuna"
+          hospitals = {
+            [
+              {
+                name: "Hospital Municipal de Nova Ipixuna",
+                address: "Jerusalem - Cachoeira da Fumaça, S/n, Nova Ipixuna - PA, 68585-000"
+              }
+            ]
+          }
+        />
+        <CityOperating 
+          city="Brasil Novo"
+          hospitals = {
+            [
+              {
+                name: "Hospital Municipal Maria Jose Biancardi",
+                address: " Tv. Vinte e Oito de Abril, 110-212, Brasil Novo - PA, 68148-000"
+              }
+            ]
+          }
+        />
+        <CityOperating 
+          city="Medicilândia"
+          hospitals = {
+            [
+              {
+                name: "Hospital Municipal de Medicilândia",
+                address: "Tv. Cassandro Silvério - Medicilândia, PA, 68145-000"
+              }
+            ]
+          }
+        />
+        <CityOperating 
+          city="Uruará"
+          hospitals = {
+            [
+              {
+                name: "Hospital Municipal de Uruará",
+                address: " Av. Pará - Val-de-Caes, Uruará - PA, 68140-000"
+              }
+            ]
+          }
+        />
       </div>
     </section>
   );
